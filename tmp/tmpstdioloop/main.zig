c2280be05a4d04907e3ebc9fe5f11bd48dec162a
metaleap <metaleap@users.noreply.github.com>
2020-01-21 20:59:18 +0100

    basic stdio loop, crikey

--- tmp/tmpstdioloop/main.zig
+++ tmp/tmpstdioloop/main.zig
@@ -1,6 +1,18 @@
 const std = @import("std");
 
 pub fn main() !void {
-    std.debug.warn("Ahoy", .{});
-    return error.None;
+    var mem = std.heap.page_allocator;
+    var stdout = std.io.getStdOut().outStream();
+    var stdin = std.io.getStdIn().inStream();
+
+    var buf = std.Buffer.initNull(mem);
+    defer buf.deinit();
+    while (true) {
+        try stdin.stream.readUntilDelimiterBuffer(&buf, '\n', 987654321);
+        const inputline = buf.toOwnedSlice();
+        defer mem.free(inputline);
+        if (inputline.len == 0)
+            break;
+        try stdout.stream.print("So you say: {s}\n", .{inputline});
+    }
 }
