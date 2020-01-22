1d74ba60fffdb25efe9548b6e5d1b3acfafc3091
metaleap <metaleap@users.noreply.github.com>
2020-01-22 22:43:54 +0100

    lsp exploration (#3)

---
+++ dummylangserver/baseprotocol.zig
@@ -0,0 +1,40 @@
+const std = @import("std");
+
+pub fn forever(
+    buf: *std.ArrayList(u8),
+    onFullIncomingPayload: fn ([]const u8) void,
+) !void {
+    var stdin = std.io.getStdIn().inStream();
+    var got_content_length: ?usize = null;
+
+    while (true) {
+        buf.len += readmore: {
+            const num_bytes = try stdin.stream.read(buf.items[buf.len..]);
+            if (num_bytes == 0) return else break :readmore num_bytes;
+        };
+
+        const so_far = buf.toSliceConst();
+        if (got_content_length == null)
+            if (std.mem.indexOf(u8, so_far, "Content-Length:")) |idx|
+                if (idx == 0 or buf.items[idx - 1] == '\n') {
+                    const idx_start = idx + "Content-Length:".len;
+                    if (std.mem.indexOfScalarPos(u8, so_far, idx_start, '\n')) |idx_newline| {
+                        const str_content_length = std.mem.trim(u8, so_far[idx_start..idx_newline], " \t\r");
+                        got_content_length = try std.fmt.parseUnsigned(usize, str_content_length, 10);
+                    }
+                };
+        if (got_content_length) |content_len| {
+            if (std.mem.indexOf(u8, so_far, "\r\n\r\n")) |idx| {
+                const keep = buf.items[idx + 4 .. buf.len];
+                std.mem.copy(u8, buf.items[0..keep.len], keep);
+                try buf.ensureCapacity(content_len);
+                if (keep.len < content_len)
+                    try stdin.stream.readNoEof(buf.items[keep.len .. content_len - keep.len]);
+                buf.len = content_len;
+                onFullIncomingPayload(buf.toSliceConst());
+                got_content_length = null;
+                buf.len = 0;
+            }
+        }
+    }
+}
--- dummylangserver/main.zig
+++ dummylangserver/main.zig
@@ -5,29 +5,14 @@ pub fn main() !u8 {
     defer mem_global.deinit();
 
     var stdout = std.io.getStdOut().outStream();
-    var stdin = std.io.getStdIn().inStream();
 
-    var buf = try std.ArrayList(u8).initCapacity(&mem_global.allocator, 8 * 1024 * 1024);
-    const needle_header_content_length = "Content-Length:";
-    var got_content_length: ?usize = null;
-    while (true) {
-        const len = try stdin.stream.read(buf.items[buf.len..]);
-        if (len == 0) break else {
-            buf.len += len;
-            const so_far = buf.toSliceConst();
-            if (got_content_length == null)
-                if (std.mem.indexOf(u8, so_far, needle_header_content_length)) |idx|
-                    if (idx == 0 or buf.items[idx - 1] == '\n') {
-                        const idx_start = idx + needle_header_content_length.len;
-                        if (std.mem.indexOfScalarPos(u8, so_far, idx_start, '\n')) |idx_sep| {
-                            const str_content_length = std.mem.trim(u8, so_far[idx_start..idx_sep], " \t\r");
-                            got_content_length = try std.fmt.parseUnsigned(usize, str_content_length, 10);
-                        }
-                    };
-            if (got_content_length) |content_len| {
-                std.debug.warn("Your clen:\t'{d}'\n", .{content_len});
-            }
-        }
-    }
+    try @import("./baseprotocol.zig").forever(
+        &try std.ArrayList(u8).initCapacity(&mem_global.allocator, 8 * 1024 * 1024),
+        onFullIncomingPayload,
+    );
     return 1;
+}
+
+fn onFullIncomingPayload(raw_json_bytes: []const u8) void {
+    std.debug.warn(">>>{s}<<<\n", .{raw_json_bytes});
 }
