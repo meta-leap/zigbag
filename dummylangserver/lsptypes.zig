0085b569b3db3d8911f73b0ae353ab684f141161
metaleap <metaleap@users.noreply.github.com>
2020-01-23 15:49:28 +0100

    lsp type scaffolds (#1)

--- dummylangserver/lsptypes.zig
+++ dummylangserver/lsptypes.zig
@@ -1,19 +1,28 @@
-const string = []const u8;
+const String = []const u8;
 
-const numberOrString = union(enum) {
+pub const JsonAny = union(enum) {
+    string: String,
+    boolean: bool,
+    int: isize,
+    float: f64,
+    array: []JsonAny,
+    object: std.StringHashMap(JsonAny),
+};
+
+const IntOrString = union(enum) {
     number: isize,
-    string: string,
+    string: String,
 };
 
 pub const Message = struct {
-    jsonrpc: string = "2.0",
+    jsonrpc: String = "2.0",
 };
 
 pub const RequestMessage = struct {
     Message: Message,
 
-    id: numberOrString,
-    method: string,
+    id: IntOrString,
+    method: String,
     params: ?union(enum) {
         initialize: *InitializeParams,
     },
@@ -22,11 +31,9 @@ pub const RequestMessage = struct {
 pub const ResponseMessage = struct {
     Message: Message,
 
-    id: ?numberOrString,
+    id: ?IntOrString,
     result: ?union(enum) {
-        number: f64,
-        string: string,
-        boolean: bool,
+        initialize: *InitializeResult,
     },
     err: ?ResponseError,
 };
@@ -46,25 +53,22 @@ pub const ResponseError = struct {
         requestCancelled = -32800,
         contentModified = -32801,
     },
-    message: string,
+    message: String,
 };
 
 pub const NotificationMessage = struct {
     Message: Message,
 
-    method: string,
+    method: String,
     params: ?union(enum) {
         __cancelRequest: *CancelParams,
     },
 };
 
-pub const DocumentUri = string;
+pub const DocumentUri = String;
 
 pub const Position = struct {
-    /// Line position in a document (zero-based).
     line: isize,
-
-    /// Character offset on a line in a document (zero-based).
     character: isize,
 };
 
@@ -79,22 +83,9 @@ pub const Location = struct {
 };
 
 pub const LocationLink = struct {
-    /// Span of the origin of this link.
-    ///
-    /// Used as the underlined span for mouse interaction. Defaults to the word range at
-    /// the mouse position.
     originSelectionRange: ?Range,
-
-    /// The target resource identifier of this link.
     targetUri: DocumentUri,
-
-    /// The full target range of this link. If the target for example is a symbol then target range is the
-    /// range enclosing this symbol not including leading/trailing whitespace but everything else
-    /// like comments. This information is typically used to highlight the range in the editor.
     targetRange: Range,
-
-    /// The range that should be selected and revealed when this link is being followed, e.g the name of a function.
-    /// Must be contained by the the `targetRange`. See also `DocumentSymbol#range`
     targetSelectionRange: Range,
 };
 
@@ -106,26 +97,26 @@ pub const Diagnostic = struct {
         information = 3,
         hint = 4,
     },
-    code: ?numberOrString,
-    source: ?string,
-    message: string,
+    code: ?IntOrString,
+    source: ?String,
+    message: String,
     relatedInformation: ?[]DiagnosticRelatedInformation,
 };
 
 pub const DiagnosticRelatedInformation = struct {
     location: Location,
-    message: string,
+    message: String,
 };
 
 pub const Command = struct {
-    title: string,
-    command: string,
-    arguments: ?[]void,
+    title: String,
+    command: String,
+    arguments: ?[]JsonAny,
 };
 
 pub const TextEdit = struct {
     range: Range,
-    newText: string,
+    newText: String,
 };
 
 pub const TextDocumentEdit = struct {
@@ -148,7 +139,7 @@ pub const CreateFileOptions = struct {
 };
 
 pub const CreateFile = struct {
-    kind: string = "create",
+    kind: String = "create",
     uri: DocumentUri,
     options: ?CreateFileOptions,
 };
@@ -159,7 +150,7 @@ pub const RenameFileOptions = struct {
 };
 
 pub const RenameFile = struct {
-    kind: string = "rename",
+    kind: String = "rename",
     oldUri: DocumentUri,
     newUri: DocumentUri,
     options: ?RenameFileOptions,
@@ -171,7 +162,7 @@ pub const DeleteFileOptions = struct {
 };
 
 pub const DeleteFile = struct {
-    kind: string = "delete",
+    kind: String = "delete",
     uri: DocumentUri,
     options: ?DeleteFileOptions,
 };
@@ -188,45 +179,284 @@ pub const WorkspaceEdit = struct {
 
 pub const TextDocumentItem = struct {
     uri: DocumentUri,
-    languageId: string,
+    languageId: String,
     version: isize,
-    text: string,
+    text: String,
 };
 
 pub const DocumentFilter = struct {
-    language: ?string,
-    scheme: ?string,
-    pattern: ?string,
+    language: ?String,
+    scheme: ?String,
+    pattern: ?String,
 };
 
 pub const DocumentSelector = []DocumentFilter;
 
-pub const MarkupKind = enum {
-    plaintext,
-    markdown,
+pub const markup_kind = struct {
+    pub const plaintext = "plaintext";
+    pub const markdown = "markdown";
 };
 
 pub const MarkupContent = struct {
-    kind: MarkupKind,
-    value: string,
+    kind: string,
+    value: String,
 };
 
 pub const InitializeParams = struct {
     processId: ?isize,
     rootUri: ?DocumentUri,
+    initializationOptions: ?JsonAny,
     capabilities: ClientCapabilities,
-    trace: ?enum {
-        off,
-        messages,
-        verbose,
-    },
+    trace: ?String,
     workspaceFolders: ?[]WorkspaceFolder,
+
+    pub const trace_off = "off";
+    pub const trace_messages = "messages";
+    pub const trace_verbose = "verbose";
 };
 
-pub const ResourceOperationKind = enum {};
+pub const WorkspaceClientCapabilities = struct {
+    applyEdit: ?bool,
+    workspaceEdit: ?struct {
+        documentChanges: ?bool,
+        resourceOperations: ?[]String,
+        failureHandling: ?String,
+
+        pub const resource_operation_kind_create = "create";
+        pub const resource_operation_kind_rename = "rename";
+        pub const resource_operation_kind_delete = "delete";
+        pub const failure_handling_kind_abort = "abort";
+        pub const failure_handling_kind_transactional = "transactional";
+        pub const failure_handling_kind_undo = "undo";
+        pub const failure_handling_kind_textOnlyTransactional = "textOnlyTransactional";
+    },
+    didChangeConfiguration: ?struct {
+        dynamicRegistration: ?bool,
+    },
+    didChangeWatchedFiles: ?struct {
+        dynamicRegistration: ?bool,
+    },
+    symbol: ?struct {
+        dynamicRegistration: ?bool,
+        symbolKind: ?struct {
+            valueSet: ?[]SymbolKind,
+        },
+    },
+    executeCommand: ?struct {
+        dynamicRegistration: ?bool,
+    },
+    workspaceFolders: ?bool,
+    configuration: ?bool,
+};
+
+pub const TextDocumentClientCapabilities = struct {
+    synchronization: ?struct {
+        dynamicRegistration: ?bool,
+        willSave: ?bool,
+        willSaveWaitUntil: ?bool,
+        didSave: ?bool,
+    },
+    completion: ?struct {
+        dynamicRegistration: ?bool,
+        completionItem: ?struct {
+            snippetSupport: ?bool,
+            commitCharactersSupport: ?bool,
+            documentationFormat: ?[]String,
+            deprecatedSupport: ?bool,
+            preselectSupport: ?bool,
+        },
+        completionItemKind: ?struct {
+            valueSet: ?[]CompletionItemKind,
+        },
+        contextSupport: ?bool,
+    },
+    hover: ?struct {
+        dynamicRegistration: ?bool,
+        contentFormat: ?[]String,
+    },
+    signatureHelp: ?struct {
+        dynamicRegistration: ?bool,
+        signatureInformation: ?struct {
+            documentationFormat: ?[]String,
+            parameterInformation: ?struct {
+                labelOffsetSupport: ?bool,
+            },
+        },
+    },
+    references: ?struct {
+        dynamicRegistration: ?bool,
+    },
+    documentHighlight: ?struct {
+        dynamicRegistration: ?bool,
+    },
+    documentSymbol: ?struct {
+        dynamicRegistration: ?bool,
+        symbolKind: ?struct {
+            valueSet: ?[]SymbolKind,
+        },
+        hierarchicalDocumentSymbolSupport: ?bool,
+    },
+    formatting: ?struct {
+        dynamicRegistration: ?bool,
+    },
+    rangeFormatting: ?struct {
+        dynamicRegistration: ?bool,
+    },
+    onTypeFormatting: ?struct {
+        dynamicRegistration: ?bool,
+    },
+    declaration: ?struct {
+        dynamicRegistration: ?bool,
+        linkSupport: ?bool,
+    },
+    definition: ?struct {
+        dynamicRegistration: ?bool,
+        linkSupport: ?bool,
+    },
+    typeDefinition: ?struct {
+        dynamicRegistration: ?bool,
+        linkSupport: ?bool,
+    },
+    implementation: ?struct {
+        dynamicRegistration: ?bool,
+        linkSupport: ?bool,
+    },
+    codeAction: ?struct {
+        dynamicRegistration: ?bool,
+        codeActionLiteralSupport: ?struct {
+            codeActionKind: struct {
+                valueSet: []String,
+            },
+        },
+    },
+    codeLens: ?struct {
+        dynamicRegistration: ?bool,
+    },
+    documentLink: ?struct {
+        dynamicRegistration: ?bool,
+    },
+    colorProvider: ?struct {
+        dynamicRegistration: ?bool,
+    },
+    rename: ?struct {
+        dynamicRegistration: ?bool,
+        prepareSupport: ?bool,
+    },
+    publishDiagnostics: ?struct {
+        relatedInformation: ?bool,
+    },
+    foldingRange: ?struct {
+        dynamicRegistration: ?bool,
+        rangeLimit: ?bool,
+        lineFoldingOnly: ?bool,
+    },
+};
+
+pub const code_action_kind = struct {
+    pub const quickfix = "quickfix";
+    pub const refactor = "refactor";
+    pub const refactor_extract = "refactor.extract";
+    pub const refactor_inline = "refactor.inline";
+    pub const refactor_rewrite = "refactor.rewrite";
+    pub const source = "source";
+    pub const source_organizeImports = "source.organizeImports";
+};
+
+pub const CompletionItemKind = enum {
+    Text = 1,
+    Method = 2,
+    Function = 3,
+    Constructor = 4,
+    Field = 5,
+    Variable = 6,
+    Class = 7,
+    Interface = 8,
+    Module = 9,
+    Property = 10,
+    Unit = 11,
+    Value = 12,
+    Enum = 13,
+    Keyword = 14,
+    Snippet = 15,
+    Color = 16,
+    File = 17,
+    Reference = 18,
+    Folder = 19,
+    EnumMember = 20,
+    Constant = 21,
+    Struct = 22,
+    Event = 23,
+    Operator = 24,
+    TypeParameter = 25,
+};
+
+pub const SymbolKind = enum {
+    File = 1,
+    Module = 2,
+    Namespace = 3,
+    Package = 4,
+    Class = 5,
+    Method = 6,
+    Property = 7,
+    Field = 8,
+    Constructor = 9,
+    Enum = 10,
+    Interface = 11,
+    Function = 12,
+    Variable = 13,
+    Constant = 14,
+    String = 15,
+    Number = 16,
+    Boolean = 17,
+    Array = 18,
+    Object = 19,
+    Key = 20,
+    Null = 21,
+    EnumMember = 22,
+    Struct = 23,
+    Event = 24,
+    Operator = 25,
+    TypeParameter = 26,
+};
+
+pub const ClientCapabilities = struct {
+    workspace: ?WorkspaceClientCapabilities,
+    textDocument: ?TextDocumentClientCapabilities,
+    experimental: ?JsonAny,
+};
+
+pub const InitializeResult = struct {
+    capabilities: ServerCapabilities,
+};
+
+pub const TextDocumentSyncKind = enum {
+    None = 0,
+    Full = 1,
+    Incremental = 2,
+};
+
+pub const CompletionOptions = struct {
+    resolveProvider: ?bool,
+    triggerCharacters: ?[]String,
+};
+
+pub const SignatureHelpOptions = struct {
+    triggerCharacters: ?[]String,
+};
+
+pub const CodeActionOptions = struct {
+    codeActionKinds: ?[]String,
+};
+
+pub const WorkspaceFolder = struct {
+    uri: DocumentUri,
+    name: String,
+};
+
+pub const ServerCapabilities = struct {};
 
 pub const CancelParams = struct {
-    id: numberOrString,
+    id: IntOrString,
 };
 
 pub const TextDocumentPositionParams = struct {
