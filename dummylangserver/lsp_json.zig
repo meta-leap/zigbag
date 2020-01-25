d4efd03a4c47710c3b615d3f4c5d22dd327dc5c4
metaleap <metaleap@users.noreply.github.com>
2020-01-26 00:52:03 +0100

    odds and ends

--- dummylangserver/lsp_json.zig
+++ dummylangserver/lsp_json.zig
@@ -1,8 +1,45 @@
 const std = @import("std");
 
+const dbg = std.debug.warn;
+
 const types = @import("./lsp_types.zig");
 
-pub fn loadOther(comptime T: type, mem: *std.heap.ArenaAllocator, from: *const std.json.Value) std.mem.Allocator.Error!?T {
+pub fn marshal(mem: *std.heap.ArenaAllocator, from: var) std.mem.Allocator.Error!std.json.Value {
+    const T = comptime @TypeOf(from);
+    const type_id = comptime @typeId(T);
+    if (T == types.IntOrString)
+        return switch (from) {
+            .int => |it| .{ .Integer = it },
+            .string => |it| .{ .String = it },
+        }
+    else if (T == types.String)
+        return .{ .String = from }
+    else if (type_id == .Bool)
+        return .{ .Bool = from }
+    else if (type_id == .Int or type_id == .ComptimeInt)
+        return std.json.Value{ .Integer = @intCast(i64, from) }
+    else if (type_id == .Float or type_id == .ComptimeFloat)
+        return .{ .Float = from }
+    else if (type_id == .Null)
+        return .{ .Null = .{} }
+    else if (type_id == .Enum)
+        return .{ .Integer = @enumToInt(from) }
+    else if (type_id == .Optional)
+        return if (from) |it| try marshal(mem, it) else .{ .Null = .{} }
+    else if (type_id == .Pointer) {
+        if (type_info.Pointer.size != .Slice)
+            return try marshal(mem, from.*)
+        else {
+            var ret: std.json.Value = .{
+                .Array = std.json.Array.initCapacity(&mem.allocator, from.len),
+            };
+            return ret;
+        }
+    } else
+        @compileError("unsupported type: " ++ @typeName(T));
+}
+
+pub fn unmarshal(comptime T: type, mem: *std.heap.ArenaAllocator, from: *const std.json.Value) std.mem.Allocator.Error!?T {
     const type_id = comptime @typeId(T);
     const type_info = comptime @typeInfo(T);
     if (T == types.IntOrString)
@@ -60,7 +97,7 @@ pub fn loadOther(comptime T: type, mem: *std.heap.ArenaAllocator, from: *const s
             .Array => |jlist| load_all: {
                 var arr = try mem.allocator.alloc(types.JsonAny, jlist.len);
                 for (jlist.items[0..jlist.len]) |*item, i|
-                    arr[i] = (try loadOther(types.JsonAny, mem, item)) orelse return null;
+                    arr[i] = (try unmarshal(types.JsonAny, mem, item)) orelse return null;
                 break :load_all .{ .array = &[_]types.JsonAny{} };
             },
             .Object => |jmap| load_all: {
@@ -68,22 +105,22 @@ pub fn loadOther(comptime T: type, mem: *std.heap.ArenaAllocator, from: *const s
                 try obj.initCapacity(@intCast(usize, std.math.ceilPowerOfTwoPromote(usize, jmap.count())));
                 var iter = jmap.iterator();
                 while (iter.next()) |pair|
-                    _ = try obj.put(pair.key, (try loadOther(types.JsonAny, mem, &pair.value)) orelse return null);
+                    _ = try obj.put(pair.key, (try unmarshal(types.JsonAny, mem, &pair.value)) orelse return null);
                 break :load_all .{ .array = &[_]types.JsonAny{} };
             },
         }
     else if (type_id == .Optional) switch (from.*) {
         .Null => return null,
-        else => return (try loadOther(type_info.Optional.child, mem, from)) orelse null,
+        else => return (try unmarshal(type_info.Optional.child, mem, from)) orelse null,
     } else if (type_id == .Pointer) {
         if (type_info.Pointer.size != .Slice) {
-            const copy = try loadOther(type_info.Pointer.child, mem, from);
+            const copy = try unmarshal(type_info.Pointer.child, mem, from);
             return try @import("./xstd.mem.zig").enHeap(&mem.allocator, copy orelse return null);
         } else switch (from.*) {
             .Array => |jarr| {
                 var ret = try mem.allocator.alloc(type_info.Pointer.child, jarr.len);
                 for (jarr.items[0..jarr.len]) |*jval, i|
-                    ret[i] = (try loadOther(type_info.Pointer.child, mem, jval)) orelse return null;
+                    ret[i] = (try unmarshal(type_info.Pointer.child, mem, jval)) orelse return null;
                 return ret;
             },
             else => return null,
@@ -97,22 +134,25 @@ pub fn loadOther(comptime T: type, mem: *std.heap.ArenaAllocator, from: *const s
                     i -= 1;
                     const field_name = @memberName(T, i);
                     const field_type = @memberType(T, i);
-                    if (comptime std.mem.eql(u8, field_name, @typeName(field_type)))
-                        @field(ret, field_name) = (try loadOther(field_type, mem, from)) orelse return null
-                    else if (jmap.getValue(std.mem.trimRight(u8, field_name, "_"))) |*jval|
-                        @field(ret, field_name) = (try loadOther(field_type, mem, jval)) orelse return null;
+                    if (comptime std.mem.eql(u8, field_name, @typeName(field_type))) {
+                        if (try unmarshal(field_type, mem, from)) |it|
+                            @field(ret, field_name) = it;
+                    } else if (jmap.getValue(comptime std.mem.trimRight(u8, field_name, "_"))) |*jval| {
+                        if (try unmarshal(field_type, mem, jval)) |it|
+                            @field(ret, field_name) = it;
+                    }
                 }
                 return ret;
             },
             else => return null,
         }
     } else {
-        std.debug.warn("TID\t{}\n", .{type_id});
+        dbg("TID\t{}\n", .{type_id});
     }
     return null;
 }
 
-pub fn loadUnion(comptime TUnion: type, mem: *std.heap.ArenaAllocator, from: *const std.json.Value, member_name: []const u8) std.mem.Allocator.Error!?usize {
+pub fn unmarshalUnion(comptime TUnion: type, mem: *std.heap.ArenaAllocator, from: *const std.json.Value, member_name: []const u8) std.mem.Allocator.Error!?usize {
     if (@typeId(TUnion) != .Union)
         @compileError("union type expected for 'TUnion'");
     comptime var i = @memberCount(TUnion);
@@ -122,7 +162,7 @@ pub fn loadUnion(comptime TUnion: type, mem: *std.heap.ArenaAllocator, from: *co
             const TMember = @memberType(TUnion, i);
             return if (@typeId(TMember) == .Void)
                 0
-            else if (try loadOther(TMember, mem, from)) |ptr|
+            else if (try unmarshal(TMember, mem, from)) |ptr|
                 @ptrToInt(ptr)
             else
                 null;
--- dummylangserver/lsp_loop.zig
+++ dummylangserver/lsp_loop.zig
@@ -74,7 +74,7 @@ fn handleFullIncomingJsonPayload(self: *Engine, raw_json_bytes: []const u8) !voi
             const msg_id = hashmap.getValue("id");
             const msg_name = hashmap.getValue("method");
             if (msg_id) |*id_jsonval| {
-                if (try json.loadOther(types.IntOrString, &mem_keep, id_jsonval)) |id| {
+                if (try json.unmarshal(types.IntOrString, &mem_keep, id_jsonval)) |id| {
                     if (msg_name) |jstr| switch (jstr) {
                         .String => |method_name| try handleIncomingRequestMsg(self, &mem_keep, id, method_name, hashmap.getValue("params")),
                         else => {},
@@ -87,13 +87,14 @@ fn handleFullIncomingJsonPayload(self: *Engine, raw_json_bytes: []const u8) !voi
             };
         },
     }
-    _ = try json.loadOther(types.JsonAny, &mem_keep, &json_tree.root); // TEMP!
 }
 
 fn handleIncomingRequestMsg(self: *Engine, mem: *std.heap.ArenaAllocator, id: types.IntOrString, method: []const u8, params: ?std.json.Value) !void {
     std.debug.warn("REQ\t{}\t{}\t{}\n", .{ id, method, params });
     const union_member_name = @import("./xstd.mem.zig").replaceScalar(u8, try std.mem.dupe(&mem.allocator, u8, method), "$/", '_');
-    const req = if (params) |*p| json.loadUnion(types.RequestIn, mem, p, union_member_name) else null;
+    const req = if (params) |*p| try json.unmarshalUnion(types.RequestIn, mem, p, union_member_name) else null;
+    std.debug.warn("NAME\t{}\t{}\n", .{ union_member_name, req });
+    if (req) |p| _ = try json.marshal(mem, p);
 }
 
 fn handleIncomingResponseMsg(self: *Engine, mem: *std.heap.ArenaAllocator, id: types.IntOrString) void {
